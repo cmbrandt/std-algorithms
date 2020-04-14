4856581c973df65a94218f3ac6205d11f73583b3
Christopher Brandt <cmb199@zips.uakron.edu>
2020-04-14 17:39:00 -0500

    Updating functional.hxx header file

--- include/functional.hxx
+++ include/functional.hxx
@@ -4,7 +4,32 @@
 #define FUNCTIONAL_HXX
 
 
+// Arithmetic operations
+//#include "functional/plus.hxx"
+//#include "functional/minus.hxx"
+//#include "functional/multiplies.hxx"
+//#include "functional/divides.hxx"
+//#include "functional/modulus.hxx"
+//#include "functional/negate.hxx"
+
+// Comparisons
+#include "functional/equal_to.hxx"
+#include "functional/not_equal_to.hxx"
+#include "functional/greater.hxx"
+#include "functional/greater_equal.hxx"
 #include "functional/less.hxx"
+#include "functional/less_equal.hxx"
+
+// Logical operations
+//#include "functional/logical_and.hxx"
+//#include "functional/logical_or.hxx"
+//#include "functional/logical_not.hxx"
+
+// Bitwise operations
+//#include "functional/bit_and.hxx"
+//#include "functional/bit_or.hxx"
+//#include "functional/bit_xor.hxx"
+//#include "functional/bit_not.hxx"
 
 
 #endif
