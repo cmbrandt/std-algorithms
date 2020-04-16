5abd6b9a14686501cee7a82704afaf7816314c24
Christopher Brandt <cmb199@zips.uakron.edu>
2020-04-16 17:47:24 -0500

    updating numeric.hxx header file

--- include/numeric.hxx
+++ include/numeric.hxx
@@ -3,7 +3,20 @@
 #ifndef NUMERIC_HXX
 #define NUMERIC_HXX
 
-
+//#include "accumulate.hxx"
+//#include "reduce.hxx"
+//#include "inner_product.hxx"
+//#include "transform_reduce.hxx"
+//#include "partial_sum.hxx"
+//#include "exclusive_scan.hxx"
+//#include "inclusive_scan.hxx"
+//#include "transform_exclusive_scan.hxx"
+//#include "transform_inclusive_scan.hxx"
+//#include "adjacent_difference.hxx"
+//#include "iota.hxx"
+//#include "gcd.hxx"
+//#include "lcm.hxx"
+//#include "midpoint.hxx"
 
 
 #endif
