0c81f6a92ec43fc163593666f75f7b002b2e7369
Christopher Brandt <cmb199@zips.uakron.edu>
2020-04-25 18:48:12 -0500

    final updates to <numeric> examples.

--- numeric_iota.cxx
+++ numeric_iota.cxx
@@ -9,10 +9,10 @@ int main()
 {
   std::vector<double> v(8);
 
-  cmb::iota(v.begin(), v.end(), -4.5);
+  cmb::iota(v.begin(), v.end(), -3.5);
 
   for (auto i : v)
-    std::cout << " " << i; // = -4.5 -3.5 -2.5 -1.5 -0.5 0.5 1.5 2.5
+    std::cout << " " << i; // = -3.5 -2.5 -1.5 -0.5 0.5 1.5 2.5 3.5
 
   std::cout << std::endl;
 }
