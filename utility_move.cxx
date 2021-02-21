a3157adae17dec3e97398ae638d9d3f92a21b930
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-21 09:24:27 -0600

    utility_move.cxx example complete.

--- utility_move.cxx
+++ utility_move.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <utility.hxx>
 
 
 int main()
 {
+  std::string str = "xyz";
+  std::vector<std::string> v;
+ 
+  v.push_back(str);
+  std::cout << "str = " << str << std::endl; // = "xyz"
 
+  v.push_back( cmb::move(str) );
+  std::cout << "str = " << str << std::endl; // =
+ 
+  for (auto const& i : v)
+    std::cout << " " << i; // = "xyz" "xyz"
+
+  std::cout << std::endl;
 }
