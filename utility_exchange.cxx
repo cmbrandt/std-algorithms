cd0f16856e855bb9225126ee0ce6e6d9b03aa20a
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-21 09:21:13 -0600

    utility_exchange.cxx example complete.

--- utility_exchange.cxx
+++ utility_exchange.cxx
@@ -4,5 +4,13 @@
 
 int main()
 {
+  int x{0};
+  int y{5};
 
+  int z = cmb::exchange(x, 5);
+
+  std::cout <<   "x = " << x // = 5
+            << "\ny = " << y // = 5
+            << "\nz = " << z // = 0
+            << std::endl;
 }
