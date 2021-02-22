986b5241eba44e1bf7135c1a95d44a62c1bffe95
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 17:49:20 -0600

    copy_if and copy_n examples complete.

--- algorithm_copy_if.cxx
+++ algorithm_copy_if.cxx
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7 };
+  std::vector<int> s(4);
 
+  auto odd = [](int x) { return (x % 2) == 1; };
+
+  cmb::copy_if(v.begin(), v.end(), s.begin(), odd);
+
+  for (auto i : s)
+    std::cout << " " << i; // = 1, 3, 5, 7
+
+  std::cout << std::endl;
 }
--- algorithm_copy_n.cxx
+++ algorithm_copy_n.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7 };
+  std::vector<int> s(8, 8);
 
+  cmb::copy_n( v.begin(), 4, s.begin() );
+
+  for (auto i : v)
+    std::cout << " " << i; // = 0 1 2 3 4 5 6 7
+
+  std::cout << std::endl;
+
+  for (auto i : s)
+    std::cout << " " << i; // = 0 1 2 3 8 8 8 8
+
+  std::cout << std::endl;
 }
