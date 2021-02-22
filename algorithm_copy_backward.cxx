002c55c6728881b126770f4b4e35b2971c8765a6
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 17:50:47 -0600

    copy_backward example complete

--- algorithm_copy_backward.cxx
+++ algorithm_copy_backward.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7 };
+  std::vector<int> s(10, 8);
 
+  cmb::copy_backward( v.begin(), v.end(), s.end() );
+
+  for (auto i : v)
+    std::cout << " " << i; // = 0 1 2 3 4 5 6 7
+
+  std::cout << std::endl;
+
+  for (auto i : s)
+    std::cout << " " << i; // = 8 8 0 1 2 3 4 5 6 7
+
+  std::cout << std::endl;
 }
