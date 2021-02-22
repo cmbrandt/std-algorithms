c73cec3613fb9306e97891622e8ea8c7b63bd6e6
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 17:35:23 -0600

    Added impl source to <algorithm.hxx> header file, completed algorithm_copy.cxx example program.

---
+++ algorithm_move_backward.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <algorithm.hxx>
+
+
+int main()
+{
+
+}
--- include/algorithm.hxx
+++ include/algorithm.hxx
@@ -13,6 +13,52 @@ namespace cmb {
   //
   // Declarations
 
+  // Mutating Sequence Operations
+
+  template <class I1, class I2>
+  constexpr I2
+  copy(I1 first, I1 last, I2 result);
+
+  template <class I1, class Size, class I2>
+  constexpr I2
+  copy_n(I1 first, Size n, I2 result);
+
+  template <class I1, class I2, class P>
+  constexpr I2
+  copy_if(I1 first, I1 last, I2 result, P pred);
+
+  template <class I1, class I2>
+  constexpr I2
+  copy_backwards(I1 first, I1 last, I2 result);
+
+  template <class I1, class I2>
+  constexpr I2
+  move(I1 first, I1 last, I2 result);
+
+  template <class I1, class I2>
+  constexpr I2
+  move_backward(I1 first, I1 last, I2 result);
+
+  template <class I1, class I2>
+  constexpr I2
+  swap_ranges(I1 first1, I1 last1, I2 first2);
+
+  template <class I1, class I2>
+  constexpr void
+  iter_swap(I1 a, I2 b);
+
+  template <class I>
+  constexpr I
+  rotate(I first, I middle, I last);
+
+  template <class I1, class I2>
+  constexpr I2
+  rotate_copy(I1 first, I1 middle, I1 last, I2 result);
+
+
+
+  // Binary Search
+
   template <class I, class T>
   constexpr I
   lower_bound(I first, I last, T const& value);
@@ -40,6 +86,234 @@ namespace cmb {
   template <class I, class T, class C>
   constexpr bool
   binary_search(I first, I last, T const& value, C compare);
+
+
+
+  namespace detail {
+
+    //
+    // Non-public set of helper functions used by copy_n to
+    // convert template parameter Size to an integral type
+
+    constexpr int
+    size_to_integer(int n)                { return n; }
+    constexpr unsigned
+    size_to_integer(unsigned n)           { return n; }
+    constexpr long
+    size_to_integer(long n)               { return n; }
+    constexpr unsigned long
+    size_to_integer(unsigned long n)      { return n; }
+    constexpr long long
+    size_to_integer(long long n)          { return n; }
+    constexpr unsigned long long
+    size_to_integer(unsigned long long n) { return n; }
+    constexpr long long
+    size_to_integer(float n)              { return (long long)n; }
+    constexpr long long
+    size_to_integer(double n)             { return (long long)n; }
+    constexpr long long
+    size_to_integer(long double n)        { return (long long)n; }
+
+  } // namespace detail
+
+
+
+  //
+  // Copy
+
+  template <class I1, // I1 models InputIterator
+            class I2> // I2 models OutputIterator
+  constexpr I2
+  copy(I1 first, I1 last, I2 result)
+  {
+    for (; first != last; ++first, (void)++result)
+      *result = *first;
+  
+    return result;
+  }
+
+
+  //
+  // Copy N
+
+  template <class I1,   // I1 models InputIterator
+            class Size, // Size is convertible to an integral type
+            class I2>   // I2 models OutputIterator
+  constexpr I2
+  copy_n(I1 first, Size n, I2 result)
+  {
+    auto nn = cmb::detail::size_to_integer(n);
+
+    if (nn > 0) {
+      *result = *first;
+      ++result;
+    
+      for (--nn; nn > 0; --nn) {
+        ++first;
+        *result = *first;
+        ++result;
+      }
+    }
+  
+    return result;
+  }
+
+
+
+  //
+  // Copy If
+
+  template <class I1, // I1 models InputIterator
+            class I2, // I2 models OutputIterator
+            class P>  // P  models UnaryPredicate
+  constexpr I2
+  copy_if(I1 first, I1 last, I2 result, P pred)
+  {
+    for (; first != last; ++first)
+      if ( pred(*first) ) {
+        *result = *first;
+        ++result;
+      }
+
+    return result;
+  }
+
+
+
+  //
+  // Copy Backwards
+
+  template <class I1, // I1 models BidirectionalIterator
+            class I2> // I2 models BidirectionalIterator
+  constexpr I2
+  copy_backward(I1 first, I1 last, I2 result)
+  {
+    while (first != last)
+      *--result = *--last;
+
+    return result;
+  }
+
+
+
+  //
+  // Move
+  
+  template <class I1, // I1 models InputIterator
+            class I2> // I2 models OutputIterator
+  constexpr I2
+  move(I1 first, I1 last, I2 result)
+  {
+    for (; first != last; ++first, (void)++result)
+      *result = cmb::move(*first);
+
+    return result;
+  }
+
+
+
+  //
+  // Move Backward
+
+  template <class I1, // I1 models BidirectionalIterator
+            class I2> // I2 models BidirectionalIterator
+  constexpr I2
+  move_backward(I1 first, I1 last, I2 result)
+  {
+    while (first != last)
+      *--result = cmb::move(*--last);
+
+    return result;
+  }
+
+
+
+  //
+  // Swap Ranges
+
+  template <class I1, // I1 models ForwardIterator
+            class I2> // I2 models ForwardIterator
+  constexpr I2
+  swap_ranges(I1 first1, I1 last1, I2 first2)
+  {
+    for (; first1 != last1; ++first1, (void)++first2)
+      cmb::iter_swap(first1, first2);
+  
+    return first2;
+  }
+
+
+
+
+  //
+  // Iter Swap
+
+  template <class I1, // I1 models ForwardIterator
+            class I2> // I2 models ForwardIterator
+  constexpr void
+  iter_swap(I1 a, I2 b)
+  {
+    return cmb::swap(*a, *b);
+  }
+
+
+
+  //
+  // Rotate
+  
+  template <class I> // I models ForwardIterator
+  constexpr I
+  rotate(I first, I middle, I last)
+  {
+    if (first == middle)
+      return last;
+    else if (last  == middle)
+      return first;
+
+    I i{middle};
+
+    do {
+      cmb::iter_swap(first, i);
+
+      ++first;
+      ++i;
+
+      if (first == middle)
+        middle = i;
+      else if (i == last)
+        i = middle;
+  
+    } while (i != last);
+
+    I r{first};
+
+    while (i != last) {
+      cmb::iter_swap(first, i);
+
+      ++first;
+      ++i;
+
+      if (first == middle)
+        middle = first;
+      else if (i == last)
+        i = middle;
+    }
+
+    return r;
+  }
+
+
+
+  //
+  // Rotate Copy
+
+  template <class I1, // I1 models InputIterator
+            class I2> // I2 models OutputIterator
+  constexpr I2
+  rotate_copy(I1 first, I1 middle, I1 last, I2 result)
+  {
+    return cmb::copy( first, middle, cmb::copy(middle, last, result) );
+  }
 
 
 
