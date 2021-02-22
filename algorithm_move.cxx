223c7625a97cc0c85ae61cf046b16953ecd33e5e
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 17:53:27 -0600

    move and move_backward example complete.

--- algorithm_move.cxx
+++ algorithm_move.cxx
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<std::string> v{ "abc", "def", "uvw", "xyz" };
+  std::vector<std::string> s(4);
 
+  cmb::move( v.begin(), v.end(), s.begin() );
+
+  for (auto const& i : v)
+    std::cout << " " << i; // = 
+
+  std::cout << std::endl;
+
+  for (auto const& i : s)
+    std::cout << " " << i; // = "abc" "def" "uvw" "xyz"
+
+  std::cout << std::endl;
 }
--- algorithm_move_backward.cxx
+++ algorithm_move_backward.cxx
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<std::string> v{ "abc", "def", "uvw", "xyz"               };
+  std::vector<std::string> s{ "aaa", "bbb", "ccc", "ddd", "eee", "fff" };
 
+  cmb::move_backward( v.begin(), v.end(), s.end() );
+
+  for (auto const& i : v)
+    std::cout << " " << i; // = 
+
+  std::cout << std::endl;
+
+  for (auto const& i : s)
+    std::cout << " " << i; // = "aaa" "bbb" abc" "def" "uvw" "xyz"
+
+  std::cout << std::endl;
 }
