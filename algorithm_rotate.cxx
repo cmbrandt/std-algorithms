b32a5293fc921f47b3dbd56386d5cda51d57f042
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 18:03:09 -0600

    rotate and rotate_copy example complete.

--- algorithm_rotate.cxx
+++ algorithm_rotate.cxx
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 1, 2, 3, 4, 5};
 
+  cmb::rotate( v.begin(), v.begin() + 3, v.end() );
+  
+  for (auto i : v)
+    std::cout << " " << i; // = 4, 5, 1, 2, 3
+
+  std::cout << std::endl;
 }
--- algorithm_rotate_copy.cxx
+++ algorithm_rotate_copy.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 1, 2, 3, 4, 5};
+  std::vector<int> s(5);
 
+  cmb::rotate_copy( v.begin(), v.begin() + 3, v.end(), s.begin() );
+  
+  for (auto i : v)
+    std::cout << " " << i; // = 1, 2, 3, 4, 5
+
+  std::cout << std::endl;
+
+  for (auto i : s)
+    std::cout << " " << i; // = 4, 5, 1, 2, 3
+
+  std::cout << std::endl;
 }
