49c24b1063ca1837d16642e869701aa5b185758c
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-22 18:01:01 -0600

    swap_ranges and iter_swap examples complete.

--- algorithm_iter_swap.cxx
+++ algorithm_iter_swap.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 1, 2, 3, 4 };
+  std::vector<int> s{ 0, 0, 0, 0 };
 
+  cmb::iter_swap( v.begin(), s.begin() + 2 );
+
+  for (auto i : v)
+    std::cout << " " << i; // = 0 2 3 4
+
+  std::cout << std::endl;
+
+  for (auto i : s)
+    std::cout << " " << i; // = 0 0 1 0
+
+  std::cout << std::endl;
 }
--- algorithm_swap_ranges.cxx
+++ algorithm_swap_ranges.cxx
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <algorithm.hxx>
 
 
 int main()
 {
+  std::vector<int> v{ 1, 2, 3, 4 };
+  std::vector<int> s{ 0, 0, 0, 0 };
 
+  cmb::swap_ranges( v.begin(), v.end(), s.begin() );
+
+  for (auto i : v)
+    std::cout << " " << i; // = 0 0 0 0
+
+  std::cout << std::endl;
+
+  for (auto i : s)
+    std::cout << " " << i; // = 1 2 3 4
+
+  std::cout << std::endl;
 }
